e723c764f8f164750939453370a4af0ffc1a65fc
sagarannaldas <annal7057@gmail.com>
2023-07-24 23:22:18 +0530

    1. composable text

--- app/src/main/java/com/example/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/MainActivity.kt
@@ -1,46 +1,50 @@
 package com.example.jetpackcomposedemo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Title()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Title() {
+    val context = LocalContext.current
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Hello world!",
+        fontSize = 32.sp,
+        fontFamily = FontFamily.Cursive,
+        color = colorResource(id = R.color.black),
+        modifier = Modifier.clickable {
+            Toast.makeText(context, "Title clicked ", Toast.LENGTH_SHORT).show()
+        }
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    JetpackComposeDemoTheme {
-        Greeting("Android")
+fun DefaultPreview() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Title()
     }
 }
