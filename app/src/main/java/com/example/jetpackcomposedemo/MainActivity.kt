1036b5ff7df8eba0afa63f4347d203eab2cf2b2e
sagarannaldas <annal7057@gmail.com>
2023-07-25 13:26:28 +0530

    1. lazy column as recycler view in jetpack compose

--- app/src/main/java/com/example/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.jetpackcomposedemo
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,65 +11,116 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.jetpackcomposedemo.ui.theme.JetpackComposeDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserCard()
+            UserList()
         }
     }
 }
 
+data class User(
+    val id: Int
+)
+
+val users = listOf<User>(
+    User(1),
+    User(2),
+    User(3),
+    User(4),
+    User(5),
+    User(6),
+    User(7),
+    User(8),
+    User(9)
+)
+
+
 @Composable
-fun UserCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .padding(12.dp)
-            .border(width = 1.dp, color = Color.Gray)
-            .padding(12.dp)
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.air_india),
-            contentDescription = "air india image",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(120.dp)
-                .clip(CircleShape)
-        )
-        Column {
-            Text(
-                modifier = Modifier.padding(12.dp),
-                text = stringResource(id = R.string.dummy_text)
-            )
-            Button(modifier = Modifier.padding(12.dp), onClick = {
-                // perform click action here
-            }) {
-                Text(text = "View Profile")
-            }
+fun UserList() {
+//    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+//        for (i in 1..10) {
+//            UserCard()
+//        }
+//    }
+
+    // lazycolumn works like recycler view
+    LazyColumn {
+        items(users) { user ->
+            UserCard(user = user)
+
         }
     }
+
+}
+
+@Composable
+fun UserCard(user: User) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(12.dp)
+                .padding(12.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.air_india),
+                contentDescription = "air india image",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.dummy_text),
+                    fontSize = 12.sp,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(12.dp)
+                )
+                Button(onClick = {
+                    // perform click action here
+                }) {
+                    Text(text = "View Profile ${user.id}")
+                }
+            }
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
@@ -81,6 +129,6 @@ fun DefaultPreview() {
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-        UserCard()
+        UserList()
     }
 }
